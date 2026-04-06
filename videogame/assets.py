2d66900ba02cd8e894de0d2c504df7dd3c3ba58e
NickTran15 <158344790+NickTran15@users.noreply.github.com>
2026-04-06 22:16:20 +0000

    done

---
+++ videogame/assets.py
@@ -0,0 +1,16 @@
+"DONE"
+
+"Logic to find game assets in asset dictionary"
+
+from os import path
+from .aim_assist_dict import aim_assist_asset_dict as asset_dict
+
+main_dir = path.split(path.abspath(__file__))[0]
+data_dir = path.join(main_dir, "data")
+
+def get(key):
+
+    value = asset_dict[key]
+    value = path.join(data_dir, value)
+    assert path.exists(value)
+    return value
